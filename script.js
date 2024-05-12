f7d9b1b360203c7bc96f2d8b9c8d338664f4989d
Sebastian Moreno <ppasanimalart@gmail.com>
2024-05-11 21:18:46 -0500

    Agregar declaración de variables

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Comentario en bloque */
+//Para comentar solo una linea se puede poner el doble slash
+
+console.log("Hola Mundo");
+/* Cuando son nros no es necesario ponerlo entre comillas */
+console.log(1234)
+console.log("Cualquier cosa que queramos ver en la consola")
+
+//Tipos de datos
+
+//Hay dos maneras de almacenar datos, una es let 
+
+//Declarar una variable
+let nombre = "Sebastian"
+console.log(nombre)
+nombre = "JuanSe"
+console.log(nombre)
+
+
+/* Ejemplo con la vriable numero */
+let numero1 = 7
+let numero2 = 9
+
+let resultado = numero1 + numero2
+
+console.log ("resultado:", resultado)
+
+//Declarar una constante
+const pi= 3.1416
+console.log(pi)
